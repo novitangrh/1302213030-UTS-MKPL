38465be310c34f6da32dd27fc883ddeb140b3e46
novitangrh <novitasabila70@gmail.com>
2024-04-21 15:47:08 +0700

    Menghapus komentar
    Menghapus komentar

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -34,13 +34,6 @@ public class Employee {
 		childIdNumbers = new LinkedList<String>();
 	}
 
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya
-	 * (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3:
-	 * 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-
 	public void setMonthlySalary(int grade) {
 		if (grade == 1) {
 			monthlySalary = 3000000;
@@ -99,13 +92,10 @@ public class Employee {
 	}
 
 	public int getAnnualIncomeTax() {
-
-		// Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah
-		// bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		return TaxFunction.calculateTax(this);
 	}
 
-	private int calculateMonthsWorkingInYear() {
+	public int calculateMonthsWorkingInYear() {
 		LocalDate currentDate = LocalDate.now();
 		if (currentDate.getYear() == this.dateJoined.getYear()) {
 			return currentDate.getMonthValue() - this.dateJoined.getMonthValue();
