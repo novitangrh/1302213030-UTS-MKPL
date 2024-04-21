dd2c9eafeaf86bc332c29b6e477501974c30e95d
novitangrh <novitasabila70@gmail.com>
2024-04-21 14:04:13 +0700

    Memperbaiki logic setMonthlySalary
    Mengubah kondisi isForeigner menjadi nationality == Nationality.FOREIGN

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -44,17 +44,17 @@ public class Employee {
 	public void setMonthlySalary(int grade) {
 		if (grade == 1) {
 			monthlySalary = 3000000;
-			if (isForeigner) {
+			if (nationality == Nationality.FOREIGN) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
 		} else if (grade == 2) {
 			monthlySalary = 5000000;
-			if (isForeigner) {
+			if (nationality == Nationality.FOREIGN) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
 		} else if (grade == 3) {
 			monthlySalary = 7000000;
-			if (isForeigner) {
+			if (nationality == Nationality.FOREIGN) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
 		}
